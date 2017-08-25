def5278a404e21c58859f73b900e11709ad0df37
김지성 <jskim6687@paran.com>
2017-08-25 10:46:09 +0900

    기본 폼 추가

--- Banana/Banana/Form1.Designer.cs
+++ Banana/Banana/Form1.Designer.cs
@@ -29,23 +29,66 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.nmeaText = new System.Windows.Forms.TextBox();
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.closeBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.PortName = "COM12";
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // nmeaText
+            // 
+            this.nmeaText.Location = new System.Drawing.Point(13, 13);
+            this.nmeaText.Name = "nmeaText";
+            this.nmeaText.Size = new System.Drawing.Size(259, 21);
+            this.nmeaText.TabIndex = 0;
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Location = new System.Drawing.Point(13, 226);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(75, 23);
+            this.connectBtn.TabIndex = 1;
+            this.connectBtn.Text = "Connect";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            this.connectBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.Location = new System.Drawing.Point(106, 226);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(75, 23);
+            this.closeBtn.TabIndex = 2;
+            this.closeBtn.Text = "Close";
+            this.closeBtn.UseVisualStyleBackColor = true;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.connectBtn);
+            this.Controls.Add(this.nmeaText);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Banana";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.IO.Ports.SerialPort serialPort1;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.TextBox nmeaText;
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.Button closeBtn;
     }
 }
 
--- Banana/Banana/Form1.cs
+++ Banana/Banana/Form1.cs
@@ -16,5 +16,41 @@ namespace Banana
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort.PortName = "COM12";
+            serialPort.BaudRate = (int)9600;
+
+            serialPort.Open();
+            if (serialPort.IsOpen)
+            {
+            }
+            else
+            {
+
+            }
+        }
+
+        private void serialPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            this.Invoke(new EventHandler(SerialReceived));
+        }
+
+        private void SerialReceived(object sender, EventArgs e)
+        {
+            String strReceive = serialPort.ReadExisting();
+            nmeaText.Text = strReceive;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            serialPort.Close();
+        }
     }
 }
